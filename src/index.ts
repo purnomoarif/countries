1fe888b3ac283c6af44637d73cfabf382e7e4fbb
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-09-02 22:12:25 +0700

    feat: get id name description

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,22 @@ app.get("/", (c) => {
   });
 });
 
+app.get("/countries", (c) => {
+  return c.json([
+    {
+      id: 1,
+      name: "India",
+      description: "India is a country in South Asia.",
+      imageUrl: "https://example.com/india.png",
+    },
+    {
+      id: 2,
+      name: "USA",
+      description:
+        "The United States of America is a country primarily located in North America.",
+      imageUrl: "https://example.com/usa.png",
+    },
+  ]);
+});
+
 export default app;
