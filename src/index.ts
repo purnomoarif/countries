c62036708d4553a546e883bd4eed2031f598df3a
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-09-05 14:15:23 +0700

    feat: implement initial endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,42 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({
-    message: "Hello World!",
-  });
+type country = {
+  id: number;
+  name: string;
+  description?: string;
+  imageUrl?: string;
+};
+
+let countries: country[] = [
+  { id: 1, name: "Indonesia" },
+  { id: 2, name: "Malaysia" },
+  { id: 3, name: "Singapore" },
+  { id: 4, name: "Thailand" },
+  { id: 5, name: "Vietnam" },
+  { id: 6, name: "Philippines" },
+  { id: 7, name: "Brunei" },
+];
+
+app.get("/countries/:id", (c) => {
+  const id = Number(c.req.param("id"));
+  const country = countries.find((country) => country.id === id);
+  return c.json(
+    country || { message: "Country not found" },
+    country ? 200 : 404
+  );
 });
 
 app.get("/countries", (c) => {
-  return c.json([
-    {
-      id: 1,
-      name: "India",
-      description: "India is a country in South Asia.",
-      imageUrl: "https://example.com/india.png",
-    },
-    {
-      id: 2,
-      name: "USA",
-      description:
-        "The United States of America is a country primarily located in North America.",
-      imageUrl: "https://example.com/usa.png",
-    },
-  ]);
+  return c.json(countries);
 });
+
+app.post();
+
+app.delete();
+
+app.patch();
+
+app.put();
 
 export default app;
